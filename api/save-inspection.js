cb2930c93d6cbe5708e87c57124d7114fead941a
thereadymark <info@thereadymarkgroup.com>
2026-03-28 18:11:17 -0500

    Update save-inspection.js

--- api/save-inspection.js
+++ api/save-inspection.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization, x-admin-password"
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, x-admin-token"
   };
 
   Object.entries(corsHeaders).forEach(([key, value]) => {
@@ -17,9 +17,9 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const adminPassword = req.headers["x-admin-password"];
+  const adminToken = req.headers["x-admin-token"];
 
-  if (adminPassword !== process.env.ADMIN_PASSWORD) {
+  if (!adminToken) {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
